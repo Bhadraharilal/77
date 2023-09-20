bdcd3fcffbb0529dd618885104cf919b8c0db08f
Bhadraharilal <145193040+Bhadraharilal@users.noreply.github.com>
2023-09-20 21:39:28 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name="Ranbir";
+var last_name="Kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5
+var  square_root=Math.sqrt(16);
+console.log(square_root);
